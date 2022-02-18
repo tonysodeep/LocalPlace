de2a5bd3e7713bbb1d7a742d7ad9cf68207abe69
tonysodeep <hynguyen2727@gmail.com>
2022-02-18 10:08:43 +0700

    editing style

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
 
 const Users = () => {
-  return <h2>Users's work</h2>;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'tony',
+      image:
+        'https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989_1280.jpg',
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
