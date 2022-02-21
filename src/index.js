359f5c0b70bfc67fbad56f2ddbce19119fa95596
tonysodeep <hynguyen2727@gmail.com>
2022-02-21 19:21:38 +0700

    finished validation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './ReactotronConfig';
 
 ReactDOM.render(
   <React.StrictMode>
--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -34,7 +34,6 @@ const DUMMY_PLACES = [
 
 const UserPlaces = () => {
   const userId = useParams().userId;
-  console.log(userId);
   const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
   return <PlaceList items={loadedPlaces} />;
 };
